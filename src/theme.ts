8bd843bef52db21cd07336a233bac23deaaefecb
Alex Langdon <aalangdon4@gmail.com>
2021-02-01 22:30:34 +0000

    Adding initial theme

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const appTheme = createMuiTheme({
+	typography: {
+		fontFamily: [
+			"Segoe UI",
+			"sans-serif"
+		].join(","),
+		button: {
+			textTransform: 'none'
+		}
+	},
+	palette: {
+		primary: {
+			main: "#61dbfb"
+		},
+		secondary: {
+			main: "#61dbfb"
+		}
+	}
+});
