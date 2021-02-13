b4d1225d0b97211a364dfd5395a09287d9a19ff3
Alex Langdon <aalangdon4@gmail.com>
2021-02-13 17:45:46 +0000

    Adding basic sign up page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,7 +1,22 @@
+import { Button, FormGroup, TextField } from "@material-ui/core";
 import React from "react";
 
 export default function SignUpPage(): JSX.Element {
 	return (
-		<h1>Sign Up</h1>
+		<div className="container pt-5">
+			<div className="row justify-content-center">
+				<form className="col-md-6 col-lg-5 mb-4">
+					<h2>Sign Up</h2>
+					<FormGroup>
+						<TextField label="Username" type="text" variant="outlined" className="mt-3" />
+						<TextField label="Email" type="email" variant="outlined" className="mt-3" />
+						<TextField label="Password" type="password" variant="outlined" className="mt-3" />
+						<TextField label="Confirm Password" type="password" variant="outlined" className="mt-3" />
+					</FormGroup>
+					<Button type="submit" color="primary" variant="contained" className="mt-3">Sign Up</Button>
+				</form>
+				<img className="col-md-6 col-lg-5" src="http://via.placeholder.com/400x600" alt="decoration" />
+			</div>
+		</div>
 	);
 }
