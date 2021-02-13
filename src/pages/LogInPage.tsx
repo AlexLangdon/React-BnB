8254da91b8f7753c1df6165bffbe2bbe7e9571d7
Alex Langdon <aalangdon4@gmail.com>
2021-02-13 15:47:18 +0000

    Adding basic login page

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,7 +1,20 @@
+import { Button, FormGroup, TextField } from "@material-ui/core";
 import React from "react";
 
 export default function LogInPage(): JSX.Element {
 	return (
-		<h1>Log In</h1>
+		<div className="container pt-5">
+			<div className="row justify-content-center">
+				<form className="col-md-6 col-lg-5 mb-4">
+					<h2>Log In</h2>
+					<FormGroup>
+						<TextField label="Email" type="email" variant="outlined" className="mt-3" />
+						<TextField label="Password" type="password" variant="outlined" className="mt-3" />
+					</FormGroup>
+					<Button type="submit" color="primary" variant="contained" className="mt-3">Log In</Button>
+				</form>
+				<img className="col-md-6 col-lg-5" src="http://via.placeholder.com/400x600" alt="decoration" />
+			</div>
+		</div>
 	);
 }
