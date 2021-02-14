5e31e97f0773fa8c3a21d0b0ff83008eccb9d4fe
Alex Langdon <aalangdon4@gmail.com>
2021-02-14 16:46:10 +0000

    Added basic form validation to log in page

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,18 +1,44 @@
 import { Button, FormGroup, TextField } from "@material-ui/core";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import "./FormPage.scss";
 
+interface LogInFormValues {
+	email: string;
+	password: string;
+}
+
 export default function LogInPage(): JSX.Element {
+	const { register, handleSubmit, errors } = useForm();
+
+	const onSubmit: SubmitHandler<LogInFormValues> = (data: LogInFormValues) => {
+		console.log(data);
+	};
+
 	return (
 		<div className="container pt-5">
 			<div className="row justify-content-center">
-				<form className="col-md-6 col-lg-5 mb-4">
+				<form onSubmit={handleSubmit(onSubmit)} className="col-md-6 col-lg-5 mb-4">
 					<h2>Log In</h2>
 					<FormGroup>
-						<TextField label="Email" type="email" variant="outlined" className="mt-3" />
-						<TextField label="Password" type="password" variant="outlined" className="mt-3" />
+						<TextField id="email" name="email" label="Email" type="email"
+							variant="outlined" className="mt-3"
+							error={!!errors.email} helperText={errors?.email?.message}
+							inputRef={register({
+								required: "Email is required",
+								pattern: {
+									value: /^\S+@\S+$/i,
+									message: "Invalid email format"
+								}
+							})} />
+						<TextField id="password" name="password" label="Password" type="password"
+							variant="outlined" className="mt-3"
+							error={!!errors.password} helperText={errors?.password?.message}
+							inputRef={register({ required: "Password is required" })} />
 					</FormGroup>
-					<Button type="submit" color="primary" variant="contained" className="mt-3">Log In</Button>
+					<Button type="submit" color="primary" variant="contained" className="mt-3">
+						Log In
+					</Button>
 				</form>
 				<div className="col-md-6 col-lg-5">
 					<h2 className="catchphrase">Locations across the world in just a few clicks</h2>
