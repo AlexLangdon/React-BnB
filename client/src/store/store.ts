b5c7a118db94b4f3ee25386d2d20a6a1945dc6c7
Alex Langdon <aalangdon4@gmail.com>
2021-02-22 22:11:13 +0000

    Adding redux logger middleware

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,14 @@
 import { applyMiddleware, createStore, Middleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "store/root-reducer";
 
 const middleware: Array<Middleware> = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+	middleware.push(logger);
+}
 
 const store = createStore(
 	rootReducer,
