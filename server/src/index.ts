cf08043ac64edf7136ef52779f0004950217b018
Alex Langdon <aalangdon4@gmail.com>
2021-02-28 15:24:57 +0000

    Fixing order of express route declarations

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,10 @@ import router from "./routes/rentals";
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Routes
+app.use("/api/rentals", router);
+
+// Has to be declared last due to the wildcard get
 if (process.env.NODE_ENV === "production") {
 	const buildPath = path.join(__dirname, "../../client/build");
 	app.use(express.static(buildPath));
@@ -12,9 +16,6 @@ if (process.env.NODE_ENV === "production") {
 		return res.sendFile(path.resolve(buildPath, "index.html"));
 	});
 }
-
-// Routes
-app.use("/api/rentals", router);
 
 app.listen(port, () => {
 	return console.log(`server is listening on ${port}`);
