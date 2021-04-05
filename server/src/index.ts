b6482fbf27f21b2e791e9d613f6fede6e6de05ca
Alex Langdon <aalangdon4@gmail.com>
2021-04-05 16:09:51 +0100

    Connecting to mongoose DB instance on startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,25 @@
+import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
 import path from "path";
+import config from "./config";
 import rentalRouter from "./routes/rentals";
 import userRouter from "./routes/users";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+mongoose.connect(config.DB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true
+}, () => {
+	console.log("Connected to Database");
+});
+
+// Middleware
+app.use(bodyParser.json());
 
 // Routes
 app.use("/api/rentals", rentalRouter);
