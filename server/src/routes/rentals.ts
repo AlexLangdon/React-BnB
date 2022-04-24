1d26cbaefb6281c18ca1dab5e86f7e9a4b38b39a
Alex Langdon <aalangdon4@gmail.com>
2022-04-24 13:09:39 +0100

    Updating endpoints for rentals with ownership

--- server/src/routes/rentals.ts
+++ server/src/routes/rentals.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { RentalModel } from "../models/rental";
 import { CreateRentalRequest } from "react-bnb-common";
+import { allowOnlyAuthUser } from "../controller/user";
 
 const router: Router = express.Router();
 
@@ -8,10 +9,20 @@ router.get("/", async (req, res) => {
 	const { city } = req.query;
 	const query = typeof city === "string" ? { city } : {};
 
-	const allRentals = await RentalModel
+	const rentals = await RentalModel
 		.find(query)
 		.populate("image");
-	return res.json(allRentals);
+	return res.json(rentals);
+});
+
+router.get("/mine", allowOnlyAuthUser, async (req, res) => {
+	const {user} = res.locals;
+	const query = {owner: user};
+
+	const rentals = await RentalModel
+		.find(query)
+		.populate("image");
+	return res.json(rentals);
 });
 
 router.get("/:rentalId", async (req, res) => {
@@ -22,8 +33,12 @@ router.get("/:rentalId", async (req, res) => {
 	return res.json(matchingRental);
 });
 
-router.post("/create", (req, res) => {
-	const rentalData: CreateRentalRequest = req.body;
+router.post("/create", allowOnlyAuthUser, (req, res) => {
+	const reqData: CreateRentalRequest = req.body;
+	const rentalData = {
+		...reqData,
+		owner: res.locals.user
+	};
 	RentalModel.create(rentalData, (error, createdRental) => {
 		if(error) {
 			console.error("/create ERROR", error);
